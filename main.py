fe955213f2dd0a3c12861007e605c56ce5643170
Jens Lee <rxrw@me.com>
2021-02-23 18:04:46 +0800

    init

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ client = SearchClient.create(os.environ.get("ALGOLIA_INDEX_ID"), os.environ.get(
 index = client.init_index(os.environ.get("ALGOLIA_INDEX_NAME"))
 path = "{}/{}".format(os.environ.get("GITHUB_WORKSPACE"), os.environ.get("INDEX_PATH"))
 
-fp = open(path, 'r')
+with open(path, 'r') as f:
+  content = f.read()
 
-content = fp.readlines()
-
-result = json.loads(content)
-
-index.save_objects(result)
+data = json.loads(content)
 
 print("上传{}成功".format(os.environ.get("INDEX_PATH")))
